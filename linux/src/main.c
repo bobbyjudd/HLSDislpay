7babd09b3b81d6c7411d54fb05a31b9ec0571da1
Bobby Judd <bobbymjudd@gmail.com>
2020-12-30 14:35:08 -0800

    Use xrandr to query monitor configurations

--- linux/src/main.c
+++ linux/src/main.c
@@ -8,50 +8,69 @@
 #include <png.h>
 #include <X11/Xlib.h>
 #include <X11/extensions/XShm.h>
+#include <X11/extensions/Xrandr.h>
 
 int main()
 {
-    const int dim = 900;
-    const int height = dim, width = dim;
+    int height, width, xOffset, yOffset, numMonitors;
     Display *d;
-    int s;
+    Window w;
+    XRRMonitorInfo* monitorInfo;
+
     XImage *image;
     png_image png_img;
     XShmSegmentInfo shminfo;
+
     d = XOpenDisplay(NULL);
-    s = DefaultScreen(d);
+    w = DefaultRootWindow(d);
 
-    image = XShmCreateImage(d, 
-        DefaultVisual(d,0), // Use a correct visual. Omitted for brevity     
-        24,   // Determine correct depth from the visual. Omitted for brevity
-        ZPixmap, NULL, &shminfo, width, height); 
-
-    shminfo.shmid = shmget(IPC_PRIVATE,
-        image->bytes_per_line * image->height,
-        IPC_CREAT|0777);
-
-    shminfo.shmaddr = image->data = shmat(shminfo.shmid, 0, 0);
-    shminfo.readOnly = False;
-
-    XShmAttach(d, &shminfo);
-
-    XShmGetImage(d,
-        RootWindow(d,0),
-        image,
-        50,
-        50,
-        AllPlanes);
-
-    memset(&png_img, 0, sizeof(png_img));
-    png_img.opaque = NULL;                 /* Initialize to NULL, free with png_image_free */
-    png_img.version = PNG_IMAGE_VERSION;   /* Set to PNG_IMAGE_VERSION */
-    png_img.width = width;                    /* Image width in pixels (columns) */
-    png_img.height = height;                   /* Image height in pixels (rows) */
-    //png_img.format;                        /* Image format as defined below */
-    //png_img->flags;                       /* A bit mask containing informational flags */
-    //png_img->colormap_entries;
     
-    png_image_write_to_file((png_imagep)(&png_img), "test.png", False, image->data, 0, NULL);
+    
+    monitorInfo = XRRGetMonitors(d, w, True, &numMonitors);
+
+    for (int i = 0; i < numMonitors; i++)
+    {
+        XRRMonitorInfo *current = &(monitorInfo[i]);
+        char filename[20];
+        printf("%lu\n", current->name);
+        width = current->width;
+        height = current->height;
+        xOffset = current->x;
+        yOffset = current->y;
+
+        image = XShmCreateImage(d, 
+        DefaultVisual(d,0), // Use a correct visual. Omitted for brevity     
+        32,                 // Determine correct depth from the visual. Omitted for brevity
+        ZPixmap, NULL, &shminfo, width, height);
+
+        shminfo.shmid = shmget(IPC_PRIVATE,
+            image->bytes_per_line * image->height,
+            IPC_CREAT|0777);
+        
+        shminfo.shmaddr = image->data = shmat(shminfo.shmid, 0, 0);
+        shminfo.readOnly = False;
+
+        XShmAttach(d, &shminfo);
+
+        XShmGetImage(d,
+            RootWindow(d,0),
+            image,
+            xOffset,
+            yOffset,
+            AllPlanes);
+
+        memset(&png_img, 0, sizeof(png_img));
+        png_img.opaque = NULL;
+        png_img.version = PNG_IMAGE_VERSION;
+        png_img.width = width;
+        png_img.height = height;
+        png_img.format = PNG_FORMAT_BGRA;
+        
+        sprintf(filename, "test_%d.png", i);
+        png_image_write_to_file((png_imagep)(&png_img), filename, False, image->data, 0, NULL);
+        
+        XShmDetach(d, &shminfo);
+    }
 
     return 0;
 }
